a78f8f7ef73eafc33528fac459a9817ce413ee3c
lemonilemon <imlemonilemon@gmail.com>
2024-03-27 16:10:34 +0800

    minor fix

--- hw1/src/NFinTech/NFinTech.sol
+++ hw1/src/NFinTech/NFinTech.sol
@@ -76,6 +76,7 @@ contract NFinTech is IERC721 {
 
     function setApprovalForAll(address operator, bool approved) external {
         // TODO: please add your implementaiton here
+        if(operator == address(0)) revert ZeroAddress();
         _operatorApproval[msg.sender][operator] = approved;
         emit ApprovalForAll(msg.sender, operator, approved);
     }
