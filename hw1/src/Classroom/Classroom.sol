f1e1042aa03016dfd261dd59da66a3817569a38f
lemonilemon <imlemonilemon@gmail.com>
2024-03-27 11:52:55 +0800

    FintechIntro2024 - HW1 - Classroom1&2 finish

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
+    bool private registered = false;
 
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        // TODO: please aaddress public owner;dd your implementaiton here
+        if(registered) return 123;
+        else {
+            registered = true;
+            return 1000;
+        }
     }
 }
 
@@ -18,6 +24,8 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(IClassroomV2(msg.sender).isEnrolled()) return 123;
+        return 1000;
     }
 }
 
@@ -25,5 +33,9 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(gasleft() == 10000 wei) {
+            return 1000;
+        } 
+        return 123;
     }
 }
